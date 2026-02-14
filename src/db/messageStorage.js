030bfa917a57982ddb9b4d9a58d1055e91e569d8
Vaibhav Agrawal <vaibhavagrawal1902@gmail.com>
2026-02-15 02:35:42 +0530

    vdv

--- src/db/messageStorage.js
+++ src/db/messageStorage.js
@@ -1,25 +1,25 @@
-// import { chatDB } from "./chatDB";
+import { chatDB } from "./chatDB";
 
-// export async function saveMessages(messages) {
-//   const db = await chatDB;
-//   const tx = db.transaction("messages", "readwrite");
-//   messages.forEach((msg) => tx.store.put(msg));
-//   await tx.done;
-// }
+export async function saveMessages(messages) {
+  const db = await chatDB;
+  const tx = db.transaction("messages", "readwrite");
+  messages.forEach((msg) => tx.store.put(msg));
+  await tx.done;
+}
 
-// export async function getMessagesByChat(conversationId) {
-//   const db = await chatDB;
-//   return db.getAllFromIndex("messages", "conversationId", conversationId);
-// }
+export async function getMessagesByChat(conversationId) {
+  const db = await chatDB;
+  return db.getAllFromIndex("messages", "conversationId", conversationId);
+}
 
-// export async function clearMessages(conversationId) {
-//   const db = await chatDB;
-//   const tx = db.transaction("messages", "readwrite");
-//   const index = tx.store.index("conversationId");
+export async function clearMessages(conversationId) {
+  const db = await chatDB;
+  const tx = db.transaction("messages", "readwrite");
+  const index = tx.store.index("conversationId");
 
-//   for await (const cursor of index.iterate(conversationId)) {
-//     cursor.delete();
-//   }
+  for await (const cursor of index.iterate(conversationId)) {
+    cursor.delete();
+  }
 
-//   await tx.done;
-// }
+  await tx.done;
+}
